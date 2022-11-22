0478c619031e2b0d969116063f3916e014a051c4
powcoder <powcoder@163.com>
2022-11-22 12:29:38 +0800

    COMP3311 ass2 python

---
+++ q2.py
@@ -0,0 +1,30 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+# COMP3311 22T1 Ass2 ... print info about different releases for Movie
+
+import sys
+import psycopg2
+
+# define any local helper functions here
+
+# set up some globals
+
+usage = "Usage: q2.py 'PartialMovieTitle'"
+db = None
+
+# process command-line args
+
+argc = len(sys.argv)
+
+# manipulate database
+
+try:
+	db = psycopg2.connect("dbname=imdb")
+	# ... add your code here ...
+except psycopg2.Error as err:
+	print("DB error: ", err)
+finally:
+	if db:
+		db.close()
